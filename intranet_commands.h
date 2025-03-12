93717ae63f683117939b827315ea74aaeae84656
clacassa <cyprien.lacassagne@epfl.ch>
2025-03-12 21:54:34 +0100

    merged pyros registers, usage of masks

--- intranet_commands.h
+++ intranet_commands.h
@@ -6,7 +6,7 @@
  * The communication between microcontrollers uses I2C protocol.
  * The following enums define the I2C buffer registers for each slave.
  * During a transaction, these addresses will be specified after the slave own address.
- * Each data transfer is 4-byte long (32-bit)
+ * To simplify things, each transaction involves 4 bytes of data (32-bit)
  * 
  * Intranet reference: https://docs.google.com/spreadsheets/d/159JLAAPnOQsqaWHLeamU7fWXOPAsgL0OkVv0sn9sXFg/edit?gid=0#gid=0
  */
@@ -28,61 +28,65 @@
 #define PRB_MO_BC  (1 << 1)
 #define PRB_ME_B   (1 << 0)
 
+/* Pyro channels map */
+#define TRB_PYRO1 (0xFF << 0)
+#define TRB_PYRO2 (0xFF << 8)
+#define TRB_PYRO3 (0xFF << 16) 
+
 /* For boolean data type, such as WAKE_UP */
-#define ON  0xAC  // Activate
-#define OFF 0xDE  // Deactivate
+#define NET_CMD_ON  0xAC  // Activate
+#define NET_CMD_OFF 0xDE  // Deactivate
 
 /* Each transfer involves 4-bytes of data */
 #define NET_XFER_SIZE 4
 
 /* Havoc DPR */
 enum NET_REG_DPR {
-    TIMESTAMP_MAIN = 0x00, // W
-    WAKE_UP        = 0x01, // W
-    IS_WOKEN_UP    = 0x02, // R
-    PRESSURIZE     = 0x03, // W
-    ABORT          = 0x04, // W
-    PRESSURE       = 0x05, // R
-    DN_NC          = 0x06,  // R
+    TIMESTAMP_MAIN = 0x00, // -W
+    WAKE_UP        = 0x01, // -W
+    IS_WOKEN_UP    = 0x02, // R-
+    PRESSURIZE     = 0x03, // -W
+    ABORT          = 0x04, // -W
+    PRESSURE       = 0x05, // R-
+    DN_NC          = 0x06, // R-
     DPR_NB_REG
 };
 
 /* Prop Board */
 enum NET_REG_PRB {
-    TIMESTAMP_MAIN  = 0x00, // W
-    WAKE_UP         = 0x01, // W
-    IS_WOKEN_UP     = 0x02, // R
-    CLEAR_TO_IGNITE = 0x03, // W
-    FSM_PRB         = 0x04, // R
-    P_OIN           = 0x05, // R
-    T_OIN           = 0x06, // R
-    P_EIN           = 0x07, // R
-    T_EIN           = 0x08, // R
-    P_CCC           = 0x09, // R
-    T_CCC           = 0x0A, // R
-    P_CIG           = 0x0B, // R
-    T_CIG           = 0x0C, // R
-    VALVES_STATE    = 0x0D,  // R/W
+    TIMESTAMP_MAIN  = 0x00, // -W
+    WAKE_UP         = 0x01, // -W
+    IS_WOKEN_UP     = 0x02, // R-
+    CLEAR_TO_IGNITE = 0x03, // -W
+    FSM_PRB         = 0x04, // R-
+    P_OIN           = 0x05, // R-
+    T_OIN           = 0x06, // R-
+    P_EIN           = 0x07, // R-
+    T_EIN           = 0x08, // R-
+    P_CCC           = 0x09, // R-
+    T_CCC           = 0x0A, // R-
+    P_CIG           = 0x0B, // R-
+    T_CIG           = 0x0C, // R-
+    VALVES_STATE    = 0x0D, // R/W
     PRB_NB_REG
 };
 
 /* Trigger Board */
 enum NET_REG_TRB {
-    TIMESTAMP_MAIN   = 0x00, // W
-    WAKE_UP          = 0x01, // W
-    IS_WOKEN_UP      = 0x02, // R
-    CLEAR_TO_TRIGGER = 0x03, // W
-    PYRO_CH1         = 0x04, // R/W
-    PYRO_CH2         = 0x05, // R/W
-    PYRO_CH3         = 0x06, // R/W
+    TIMESTAMP_MAIN   = 0x00, // -W
+    WAKE_UP          = 0x01, // -W
+    IS_WOKEN_UP      = 0x02, // R-
+    CLEAR_TO_TRIGGER = 0x03, // -W
+    PYROS            = 0x04, // R/W
+    HAS_TRIGGERED    = 0x05, // R-
     TRB_NB_REG
 };
 
 /* Cameras */
 // enum NET_REG_CAM {
-//     TIMESTAMP_MAIN = 0x00, // W
-//     WAKE_UP        = 0x01, // W
-//     IS_WOKEN_UP    = 0x02, // R
+//     TIMESTAMP_MAIN = 0x00, // -W
+//     WAKE_UP        = 0x01, // -W
+//     IS_WOKEN_UP    = 0x02, // R-
 //     RECORDING      = 0x03  // R/W
 // };
 
