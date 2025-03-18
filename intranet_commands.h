c92c75f133f6d2c1b029e255d052406fe9fa810f
Cléo Renaud <73248361+cleorenaud@users.noreply.github.com>
2025-03-18 14:02:25 +0100

    Binary notation for better readability

--- intranet_commands.h
+++ intranet_commands.h
@@ -21,12 +21,12 @@
 #define NET_ADDR_TRB     0x0A
 
 /* Engine valves map */
-#define PRB_VO_NOC 5    // 101
-#define PRB_VE_NO  4    // 100 
-#define PRB_IO_NCC 3    // 011
-#define PRB_IE_NC  2    // 010
-#define PRB_MO_BC  1    // 001
-#define PRB_ME_B   0    // 000
+#define PRB_VO_NOC 0b101
+#define PRB_VE_NO  0b100
+#define PRB_IO_NCC 0b011
+#define PRB_IE_NC  0b010
+#define PRB_MO_BC  0b001
+#define PRB_ME_B   0b000
 
 /* Pyro channels map */
 #define TRB_PYRO1 (0xFF << 0)
